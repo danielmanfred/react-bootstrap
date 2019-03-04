62002ade7a1bf2d44af23ad3482ede33d7808150
danielmanfred <danielmanfred@gmail.com>
2019-03-04 19:21:20 -0300

    Init app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container-fluid">
+        <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+          
+          <a className="navbar-brand" href="#"><i class="fas fa-igloo"></i>&nbsp;Responsive Website</a>
+
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMainToggler"
+          aria-controls="navbarMainToggler" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <section className="collapse navbar-collapse" id="navbarMainToggler">
+            <div className="navbar-nav">
+              <a className="nav-item nav-link" href="#">Home</a>
+              <a className="nav-item nav-link" href="#">Channel</a>
+              <a className="nav-item nav-link" href="#">Login</a>
+            </div>
+          </section>
+
+        </nav>
       </div>
     );
   }
